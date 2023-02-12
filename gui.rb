c39db927fdbec71ceb70950b99d86e9f17eb7499
edelkas <edlucasma@gmail.com>
2023-02-13 00:20:13 +0100

    abstracted some widgets, added comboboxes

--- gui.rb
+++ gui.rb
@@ -3,18 +3,50 @@ require 'byebug'
 
 # Root window
 root = TkRoot.new(title: "N++ Custom Userlevel Search Engine")
+root.minsize(480, 480)
+root.geometry('480x480')
+root.grid_columnconfigure(0, weight: 1)
 
-# Variables holding values of widgets
-vTitleText = TkVariable.new("Title")
-vTitleBox  = TkVariable.new(false)
+# Custom class to hold a search filter
+class Filter
+  def initialize(parent, value, active)
+    is_list = value.is_a?(Array)
 
-# Widgets
-wTitleText  = TkEntry.new(root, textvariable: vTitleText, state: vTitleBox == true ? "normal" : "disabled")
-wTitleBox   = TkCheckButton.new(root, variable: vTitleBox, command: ->{ wTitleText.state = vTitleBox == false ? "disabled" : "normal" })
+    # Create variables
+    @vText    = TkVariable.new(is_list ? value[0] : value)
+    @vCheck   = TkVariable.new(active)
+    @vEntries = TkVariable.new(value) if is_list
 
-# Layout
-wTitleBox.pack(side: "left")
-wTitleText.pack(side: "left")
+    # Create widgets
+    @wFrame = TkFrame.new(parent)
+    @wText  = (is_list ? TkCombobox : TkEntry).new(@wFrame, textvariable: @vText, state: state)
+    @wText.values = value if is_list
+    @wCheck = TkCheckButton.new(@wFrame, variable: @vCheck, command: ->{ @wText.state = state })
+
+    # Place widgets
+    @wCheck.pack(side: 'left')
+    @wText.pack(side: 'left', fill: 'x', expand: 1)
+  end
+
+  def state
+    @vCheck == true ? "normal" : "disabled"
+  end
+
+  # Recover TK geometry methods
+  def pack(**args)  @wFrame.pack(args)  end
+  def grid(**args)  @wFrame.grid(args)  end
+  def place(**args) @wFrame.place(args) end
+end
+
+# Create search filter fields
+filters = [
+  'Title',
+  'Author',
+  ['All', 'Solo', 'Coop', 'Race']
+].map{ |f| Filter.new(root, f, false) }
+
+# Place fields
+filters.each_with_index{ |f, i| f.grid(row: i, column: 0, sticky: 'ew') }
 
 # Start GUI loop
 Tk.mainloop
