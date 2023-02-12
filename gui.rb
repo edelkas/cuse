fd3e8b10f7de2b2c44f2c5888b9f1770fc42d82a
edelkas <edlucasma@gmail.com>
2023-02-12 02:12:32 +0100

    first widgets

--- gui.rb
+++ gui.rb
@@ -1,4 +1,20 @@
 require 'tk'
+require 'byebug'
 
-root = TkRoot.new(title: "Hey")
+# Root window
+root = TkRoot.new(title: "N++ Custom Userlevel Search Engine")
+
+# Variables holding values of widgets
+vTitleText = TkVariable.new("Title")
+vTitleBox  = TkVariable.new(false)
+
+# Widgets
+wTitleText  = TkEntry.new(root, textvariable: vTitleText, state: vTitleBox == true ? "normal" : "disabled")
+wTitleBox   = TkCheckButton.new(root, variable: vTitleBox, command: ->{ wTitleText.state = vTitleBox == false ? "disabled" : "normal" })
+
+# Layout
+wTitleBox.pack(side: "left")
+wTitleText.pack(side: "left")
+
+# Start GUI loop
 Tk.mainloop
